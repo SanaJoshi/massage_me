aeb204969c351fa5fbebba30be0df818f5132fa8
Sana Joshi <sanajoshi@Sana-MacBook-1.local>
2017-11-02 15:50:51 +1100

    image issue fixed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,6 +71,6 @@ class ProductsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
 
     def product_params
-      params.require(:product).permit(:image_data, :name, :price, :discription)
+      params.require(:product).permit(:image_data, :name, :price, :discription, :image)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,6 @@
-
-
 class Product < ApplicationRecord
   belongs_to :user, optional: true
   include ImageUploader::Attachment.new(:image)
-  
+
 
 end
