aee4900b6023bcf038a277db7fa9455d607d3972
Sana Joshi <sanajoshi@Sana-MacBook-1.local>
2017-11-08 11:35:42 +1100

    stripe issues fixed

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,11 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['pk_test_ExkXsQblPwBEaenqdFmmpj6l'],
-  :secret_key      => ENV['sk_test_a9jpiJl2QrB9nB9hG3QgWzEt']
-}
+# Rails.configuration.stripe = {
+#   :publishable_key => ENV['pk_test_ExkXsQblPwBEaenqdFmmpj6l'],
+#   :secret_key      => ENV['sk_test_a9jpiJl2QrB9nB9hG3QgWzEt']
+# }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Rails.configuration.stripe = {
+  publishable_key: Rails.application.secrets.stripe_publishable_key,
+  secret_key: Rails.application.secrets.stripe_secret_key
+}
+Stripe.api_key = Rails.application.secrets.stripe_secret_key
